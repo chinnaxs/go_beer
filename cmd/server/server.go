49f7674735d9cf2049c0c310d2d9fe97979f30ca
Christian Fehlmann <chrigi.fehlmann@gmail.com>
2020-03-01 21:41:10 +0100

    add server implementation and beers endpoint

--- cmd/server/server.go
+++ cmd/server/server.go
@@ -2,11 +2,26 @@ package main
 
 import (
 	"log"
+	"net"
+	"net/http"
 
 	"github.com/chinnaxs/go_beer/internal/pkg/db"
+	"github.com/chinnaxs/go_beer/internal/pkg/server"
 )
 
 func main() {
+	tcpAddr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:8080")
+	if err != nil {
+		log.Fatal("was not able to parse tcp Addr")
+	}
+	fileDB, err := db.NewFileDB("beerstore.json")
+	if err != nil {
+		log.Fatalf("was not able to connect to database: %s", err)
+	}
+	s := server.NewServer(tcpAddr, fileDB)
+	s.Start()
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 	db, err := db.NewFileDB("test.json")
 	if err != nil {
 		log.Println(err)
